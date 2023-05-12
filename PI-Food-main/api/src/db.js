e2ead4235ba20261b3ed12a57dba07909acf968c
santiagoDeMarco <corvalansantiago29@gmail.com>
2023-05-12 04:01:32 -0300

    prueba deploy con railway

--- PI-Food-main/api/src/db.js
+++ PI-Food-main/api/src/db.js
@@ -4,10 +4,10 @@ const DietModel = require("./models/Diet");
 const RecipeModel = require("./models/Recipe.js");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food_pyvc`,
+  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/railway`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
